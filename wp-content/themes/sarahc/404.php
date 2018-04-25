c17e3ba4bc61abc7ac352942b7992ad0d6c13199
Matt <matt@mspacecreative.com>
2018-04-25 06:25:25 -0300

    c

--- wp-content/themes/sarahc/404.php
+++ wp-content/themes/sarahc/404.php
@@ -9,7 +9,7 @@
 			
 				<h1><?php _e( 'Page not found', 'html5blank' ); ?></h1>
 				<p>
-					<?php _e('It appears that this page does not exist! Please use the navigation above or'); ?><a href="<?php echo home_url(); ?>"><?php _e( 'Return home', 'html5blank' ); ?></a>
+					<?php _e('It appears that this page does not exist! Please use the navigation above or '); ?><br /><a href="<?php echo home_url(); ?>"><?php _e( 'Return home', 'html5blank' ); ?></a>
 				</p>
 			
 			</article>
